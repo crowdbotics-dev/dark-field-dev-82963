77a06fcd43a884fa4f339805a44a94c3e9d56ef3
Crowdbotics <engineering@crowdbotics.com>
2023-05-13 23:17:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-field-dev-82963",
+  url: "https://dark-field-dev-82963.botics.co",
+  api: "https://dark-field-dev-82963.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
